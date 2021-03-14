04fa17c3b7b9285d844e17c53c74a499320bfe05
Sarvarbek Nazarov <sarvarbek.nazarov@ogr.iu.edu.tr>
2021-03-15 02:14:46 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,56 @@
-#Explain your work
+print("\nCV Application")
+print("\nCV of the first candidate\n")
+CV1 = dict([
+    ("Name" , "Name of the first candidate"),
+    ("Surname" , "Surname of the first candidate"),
+    ("Address" , "Address of the first candidate"),
+    ("Work exprerience" , "Work exprerience the first candidate")
+])
+for x in CV1:
+    print (x, ',', CV1[x])
 
-#Question 1
-for x in range(a):
-	print(a)
+
+print("\nCV of the second candidate\n")
+
+CV2 = dict([
+    ("Name" , "Name of the second candidate"),
+    ("Surname" , "Surname of of the second candidate"),
+    ("Address" , "Address of the second candidate"),
+    ("Work exprerience" , "Work exprerience of the second candidate")
+])
+
+for x in CV2:
+    print (x, ',', CV2[x])
+
+print("\nCV of the third candidate\n")
+
+CV3 = dict([
+    ("Name" , "Name of the third candidate"),
+    ("Surname" , "Surname of of the third candidate"),
+    ("Address" , "Address of the third candidate"),
+    ("Work exprerience" , "Work exprerience of the third candidate")
+    ])
+for x in CV3:
+    print (x, ',', CV3[x])
+
+print("\nCV of the fourth candidate\n")
+
+CV4 = dict([
+    ("Name" , "Name of the fourth candidate"),
+    ("Surname" , "Surname of of the fourth candidate"),
+    ("Address" , "Address of the fourth candidate"),
+    ("Work exprerience" , "Work exprerience of the fourth candidate")
+])
+for x in CV4:
+    print (x, ',', CV4[x])
+
+print("\nCV of the fifth candidate\n")
+
+CV5 = dict([
+    ("Name" , "Name of the fifth candidate"),
+    ("Surname" , "Surname of of the fifth candidate"),
+    ("Address" , "Address of the fifth candidate"),
+    ("Work exprerience" , "Work exprerience of the fifth candidate")
+])
+for x in CV5:
+    print (x, ',', CV5[x])
