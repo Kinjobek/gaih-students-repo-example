1fe5fbb91c55dfbc6ed872d984aedb053399079a
Sarvarbek Nazarov <sarvarbek.nazarov@ogr.iu.edu.tr>
2021-03-15 02:14:17 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,24 @@
-#Explain your work
+nums = list(range(21))
 
-#Question 1
-for x in range(a):
-	print(a)
+listOdd = []
+listEven = []
+
+for x in nums:
+	if (x % 2 == 0) : 
+		listEven.append(x)	
+	if (x % 2 != 0) :
+		listOdd.append(x)
+
+print ("The even list: ", listEven)
+print ("The odd list:  ", listOdd)
+
+newlist = listOdd + listEven
+print ("Merged list:  ", newlist)
+
+newlist = [i * 2 for i in newlist]
+newlist =sorted(newlist)
+
+print ("Every element multiplied by 2 and sorted by ascending:  ", newlist)
+
+for element in newlist:
+    print(element, type(element))
